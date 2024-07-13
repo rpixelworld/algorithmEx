0ffe5493fbe3404a897bc5907bc69cf542ba81a1
Rita Zhou <rzhou150@gmail.copm>
2024-07-12 23:07:05 -0700

    linklist

--- src/main/java/datastructures/ll/LinkedList.java
+++ src/main/java/datastructures/ll/LinkedList.java
@@ -35,6 +35,96 @@ public class LinkedList {
         return length;
     }
 
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    /*
+        append node at the end
+     */
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeLast() {
+        if(length == 0) {
+            return null;
+        }
+
+        Node point = head;
+        while (point.next!=null){
+            if(point.next == tail) {
+                Node temp = tail;
+                tail = point;
+                tail.next = null;
+                length --;
+                return temp;
+            }
+            point = point.next;
+        }
+
+        length =0;
+        head = null;
+        tail = null;
+        return point;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeFirst() {
+        if(length == 0) return null;
+
+        Node temp = head;
+        if(head.next == null) {
+            head = null;
+            tail = null;
+        }
+        else {
+            head = temp.next;
+            temp.next = null;
+        }
+        length --;
+
+        return temp;
+    }
+
+    public Node get(int index) {
+        if(index < 0 || index>=length || length==0) {
+            return null;
+        }
+
+        Node temp = head;
+        for(int i=0; i<length; i++) {
+            if(i==index){
+                break;
+            }
+            temp = temp.next;
+        }
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
--- src/test/java/datastructures/ll/LinkListTest.java
+++ src/test/java/datastructures/ll/LinkListTest.java
@@ -10,4 +10,124 @@ public class LinkListTest {
 
         myLinkedList.printAll();
     }
+
+    @Test
+    public void testAppend() {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+        myLinkedList.printAll();
+    }
+
+    @Test
+    public void testRemoveLast() {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
+
+
+    	/*
+        	EXPECTED OUTPUT:
+        	----------------
+        	2
+        	1
+        	null
+
+     	*/
+    }
+
+    @Test
+    public void testPrepend() {
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myLinkedList.printAll();
+
+        myLinkedList.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
+        myLinkedList.printAll();
+
+
+        /*
+            EXPECTED OUTPUT:
+
+            Before prepend():
+            -----------------
+            Head: 2
+            Tail: 3
+            Length: 2
+
+            Linked List:
+            2
+            3
+
+
+            After prepend():
+            ----------------
+            Head: 1
+            Tail: 3
+            Length: 3
+
+            Linked List:
+            1
+            2
+            3
+
+        */
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeFirst());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+
+    }
+
+    @Test
+    public void testGet() {
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+
+        System.out.println(myLinkedList.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+    }
 }
