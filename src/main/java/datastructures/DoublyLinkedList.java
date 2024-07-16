f06fa951869b295b0dcfacbf069249f2f1cef445
Rita Zhou <rzhou150@gmail.copm>
2024-07-16 14:43:39 -0700

    DoublyLinkedList

--- src/main/java/datastructures/DoublyLinkedList.java
+++ src/main/java/datastructures/DoublyLinkedList.java
@@ -73,7 +73,114 @@ public class DoublyLinkedList {
     }
 
     public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length==0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        length ++;
+    }
 
+    public Node removeFirst() {
+        if(length==0)   return null;
+
+        Node removed = head;
+        if(head == tail) {
+            head = null;
+            tail = null;
+        }
+        else {
+            head = removed.next;
+            head.prev = null;
+            removed.next = null;
+        }
+        length --;
+        return removed;
+
+    }
+
+    public Node get(int index) {
+
+        if (index<0 || index>=length) return null;
+        Node temp = null;
+
+        for(int i=0; i<=index; i++) {
+            temp = (temp==null?head:temp.next);
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+
+        if (index<0 || index>=length) return false;
+        Node temp = null;
+
+        for(int i=0; i<=index; i++) {
+            temp = (temp==null?head:temp.next);
+        }
+        temp.value = value;
+        return true;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index<0 || index>length) return false;
+
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if(index == length) {
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node pre = head;
+        Node cur = head;
+
+        for (int i=0; i<index; i++) {
+            pre = cur;
+            cur = cur.next;
+        }
+        pre.next = newNode;
+        newNode.next = cur;
+        newNode.prev = pre;
+        cur.prev = newNode;
+
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index<0 || index>=length) return null;
+
+        if (index==0){
+            return removeFirst();
+        }
+
+        if(index==length-1) {
+            return removeLast();
+        }
+
+        Node pre = head;
+        Node cur = head;
+        for(int i=0; i<index;i++) {
+            pre = cur;
+            cur = cur.next;
+        }
+
+        pre.next = cur.next;
+        cur.next.prev = pre;
+        cur.prev = null;
+        cur.next = null;
+
+        length--;
+        return cur;
     }
 
     public void printList() {
